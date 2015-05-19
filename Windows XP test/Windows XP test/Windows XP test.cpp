c101f7a5765c68138aeca2e83d0022097bd6ec3f
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2015-05-19 01:45:35 -0300

    initial commit

---
+++ Windows XP test/Windows XP test/Windows XP test.cpp
@@ -0,0 +1,17 @@
+// Windows XP test.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+#include <cstdlib>
+#include <ios>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("windows xp\n\n");
+	system ("PAUSE");
+	return 0;
+}
+
